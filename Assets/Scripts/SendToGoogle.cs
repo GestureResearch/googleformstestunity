2b8aa61849ddaa94f9ce870180c0c3af8c7ed888
Paritosh <contact@iamparitoshsharma.me>
2019-01-28 19:31:34 +0530

    Added changes for coke recording sheet

--- Assets/Scripts/SendToGoogle.cs
+++ Assets/Scripts/SendToGoogle.cs
@@ -6,29 +6,33 @@ using UnityEngine.UI;
 public class SendToGoogle : MonoBehaviour
 {
 
-    public GameObject storename;
-    public Transform location;
-    public Transform status;
-    public Transform fixturetype;
-    public GameObject notes;
+    public GameObject dedicatedto;
+    public GameObject songname;
+    public GameObject qr;
+    public GameObject phone;
+    public GameObject whatsappurl;
+    public GameObject email;
 
-    private string storenametext;
-    private string locationtext;
-    private string statustext;
-    private string fixturetypetext;
-    private string notestext;
+    private string dedicatedtotext;
+    private string songnametext;
+    private string qrtext;
+    private string phonetext;
+    private string whatsappurltext;
+    private string emailotext;
+
 
     [SerializeField]
-    private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfpormSRr0aQScVTeLxHtzCgXj6Lpo3Z2jN1ClEjDFiinhXwA/formResponse";
+    private string BASE_URL = "https://docs.google.com/forms/u/4/d/e/1FAIpQLSdkUUvGvFhHmyWBf0GVEwF5W2SYbH4Jgccwpf8l4HLsc_k8sw/formResponse";
 
     IEnumerator Post()
     {
         WWWForm form = new WWWForm();
-        form.AddField("entry.45538906", storenametext);
-        form.AddField("entry.652813880", locationtext);
-        form.AddField("entry.1774739367", statustext);
-        form.AddField("entry.1809101848", fixturetypetext);
-        form.AddField("entry.1920164014", notestext);
+        form.AddField("entry.965759182", dedicatedtotext);
+        form.AddField("entry.503931887", songnametext);
+        form.AddField("entry.1504303801", qrtext);
+        form.AddField("entry.41641842", phonetext);
+        form.AddField("entry.1869660751", whatsappurltext);
+        form.AddField("entry.2121017376", emailotext);
         byte[] rawData = form.data;
         WWW www = new WWW(BASE_URL, rawData);
         yield return www;
@@ -36,11 +40,12 @@ public class SendToGoogle : MonoBehaviour
 
     public void Send() 
     {
-        storenametext = storename.GetComponent<InputField>().text;
-        locationtext = location.GetComponent<Dropdown>().options[location.GetComponent<Dropdown>().value].text;
-        statustext = status.GetComponent<Dropdown>().options[status.GetComponent<Dropdown>().value].text;
-        fixturetypetext = fixturetype.GetComponent<Dropdown>().options[fixturetype.GetComponent<Dropdown>().value].text;
-        notestext = notes.GetComponent<InputField>().text;
+        dedicatedtotext = dedicatedto.GetComponent<InputField>().text;
+        songnametext = songname.GetComponent<InputField>().text;
+        qrtext = qr.GetComponent<InputField>().text;
+        phonetext = phone.GetComponent<InputField>().text;
+        whatsappurltext = whatsappurl.GetComponent<InputField>().text;
+        emailotext = email.GetComponent<InputField>().text;
         StartCoroutine(Post());
     }
 }
